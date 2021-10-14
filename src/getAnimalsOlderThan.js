ef5017923aadeecabd8f4d681dd37dd67a3cca96
IgorHamzi <igorwhamzi@gmail.com>
2021-10-14 16:20:35 -0300

    adiciona requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const object = species.find((element) => animal.includes(element.name));
+  const ageAnimal = object.residents.every((element2) => element2.age >= age);
+  return ageAnimal;
 }
 
 module.exports = getAnimalsOlderThan;

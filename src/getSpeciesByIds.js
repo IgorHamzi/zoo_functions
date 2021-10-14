7866bf82824c76c3c3bf9fa66c3b263356bdd1e1
IgorHamzi <igorwhamzi@gmail.com>
2021-10-14 15:57:38 -0300

    adiciona requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const object = species.filter((nome) => ids.includes(nome.id));
+  if (ids.length === undefined) {
+    return [];
+  }
+  return object;
 }
+console.log(getSpeciesByIds('lionId'));
 
 module.exports = getSpeciesByIds;

a7a9cfc8d2a751b91ea8f43b3a998da1792d3160
IgorHamzi <igorwhamzi@gmail.com>
2021-10-15 11:12:17 -0300

    adiciona requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const manager = employees.some((element) => element.managers.includes(id));
+  return manager;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const subManager = employees.filter((element) => element.managers.includes(managerId));
+  const namesManager = subManager.map((element2) => `${element2.firstName} ${element2.lastName}`);
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } return namesManager;
 }
-
+getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992');
 module.exports = { isManager, getRelatedEmployees };

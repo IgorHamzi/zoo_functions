3f994a0e1c4c0328bdb6e72df535f6fad8581830
IgorHamzi <igorwhamzi@gmail.com>
2021-10-16 14:50:10 -0300

    adiciona requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const allAnimal = species.reduce((acc, currentValue) => {
+  acc[currentValue.name] = currentValue.residents.length;
+  return acc;
+}, {});
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) return allAnimal;
+  const { specie, sex } = animal;
+  const objAnimal = species.find((Element) => specie === Element.name);
+  const { residents } = objAnimal;
+  if (sex === undefined) return residents.length;
+  const sexAnimal = objAnimal.residents.filter((element2) => element2.sex === sex);
+  return sexAnimal.length;
 }
+console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
 
 module.exports = countAnimals;

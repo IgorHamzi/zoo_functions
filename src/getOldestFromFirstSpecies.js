d69afd327676340fd3552d311a94ab941efc0f80
IgorHamzi <igorwhamzi@gmail.com>
2021-10-18 19:14:14 -0300

    adiciona requisito 6

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,19 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const objPersonId = employees.find((person) => person.id === id);
+  const firstSpecie = objPersonId.responsibleFor[0];
+  const objSpecie = species.find((animal) => animal.id === firstSpecie).residents;
+  const oldAnimal = objSpecie.reduce((acc, currentValue) => {
+    if (currentValue.age > acc.age) {
+      return currentValue;
+    } return acc;
+  });
+  const { name, sex, age } = oldAnimal;
+  return [name, sex, age];
 }
+
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
